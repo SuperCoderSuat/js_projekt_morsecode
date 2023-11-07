456f653f160253b2cfcf34a2cc0c03b37a404e86
SuperCoderSuat <supercodesuat@gmail.com>
2023-11-07 15:06:56 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." },
+];
+
+const inputText = document.body.querySelector("#input_text");
+const outputText = document.body.querySelector("#output_text");
+const inputSubmit = document.body.querySelector("#input_submit");
+
+const translate = () => {
+
+    event.preventDefault();
+
+    outputText.textContent = " ";
+
+    for (let char of inputText.value.toUpperCase()) {
+
+        let charToMorse = morseAlphabet.filter((elt) => elt.letter === char);
+
+        outputText.textContent += charToMorse[0].morseCode;
+
+    }
+
+};
+
+inputSubmit.addEventListener("click", translate);
